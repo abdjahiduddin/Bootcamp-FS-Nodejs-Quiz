1a48673c00886294f7fdc384ac497591b58d89ac
abdjahiduddin <abd.jahiduddin@gmail.com>
2022-06-08 00:44:18 +0700

    Update Quiz Week 2 Day 1 - PostgreSQL

--- Week02/Day01/soal05.sql
+++ Week02/Day01/soal05.sql
@@ -6,13 +6,6 @@ WHERE department_name = 'IT'
 SELECT employee_id FROM employees
 WHERE department_id = 6
 
--- Untuk menampilkan ID dari employee yang ada di department IT dapat juga menggunakan query INNER JOIN berikut
-SELECT employees.employee_id 
-FROM employees
-INNER JOIN departments
-ON employees.department_id = departments.department_id
-WHERE departments.department_name = 'IT'
-
 -- Selanjutnya menambahkan data ke table project_assignment dengan employee yang ada di department IT
 INSERT INTO project_assignment(pras_proj_id, pras_employee_id, pras_startdate, pras_status)
 VALUES (1, 104, '2021-09-14','ASSIGNED');
